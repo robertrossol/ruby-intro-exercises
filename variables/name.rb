c79fa0e6be0c706576bd9865ee84535b8fe8b6fa
Robert Rossol <rrossol88@gmail.com>
2021-08-29 11:53:31 -0500

    Adds variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+puts "Please enter your name:"
+name = gets.chomp
+puts "Hello #{name}! It's lovely to meet you" 
+
+10.times { |n| puts name }
+
+# 10.times do
+#   puts name
+# end
---
+++ variables/name_2.rb
@@ -0,0 +1,7 @@
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your last name:"
+last_name = gets.chomp
+# puts "Hello #{first_name} #{last_name}! It's lovely to meet you" 
+#or
+puts "Hello " + first_name + " " + last_name +"! It's nice to meet you"
